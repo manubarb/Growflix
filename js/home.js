b62cf1daaed25dca431269b49b817e56ae0628c3
Manuela de Almeida Barba <manuelabarba@mpam.mp.br>
2025-11-11 14:20:22 -0400

    backend atualizado

--- js/home.js
+++ js/home.js
@@ -1,47 +1,44 @@
 import {videos} from './data.js';
 
-// retorna array por categoria
-function returnObjectArray(objectArray, category){
-    return objectArray.filter(video => video.category === category)
-}
-//let resultArray = returnObjectArray(videos,)
-
-// retorna o id do container
-function returnContainerId(category){
-     const containers = {
-        growdev: "growdev",
-        flutter: "flutter",
-        uxui: "ux-ui",
-        diversos: "diversos"
-    };
-    
-    const containerId = containers[category];
-    return document.getElementById(containerId);
+function returnObjectArray(objectArray){
+    return objectArray.filter(video => {
+        const validCategories = ["Growcast", "Flutter", "Jornada UX/UI", "Diversos"]; 
+        return validCategories.includes(video.category);
+    })
 }
 
-// cria os cards de acordo com cada categoria de array 
-function renderCards(objArray, container) {
-    const container = container;
-    container.innerHTML = ""
-
-    // processa cada objeto para colocar no card
+function createCards(objArray) {
     const card = objArray.map(obj => {
-      const { img, link } = obj
-      return `
-        <div class="row">
-                <div class="col">
-                    <img src="${img}" class="img-fluid">
-                </div>
-                <a href="${link}"></a>
-        </div>
-    `
+      const { img, link, category } = obj
+      return `<div class="col ${category}">
+                <img src="${img}" class="img-fluid">
+            </div>
+            <a href="${link}"></a>`
+    }).join('')
+
+    const row = document.createElement("div")
+    row.classList.add("row")
+    row.innerHTML = card
+
+    return row
+}
+
+function renderCards(){
+    const cardsArray = returnObjectArray(videos)
+    const categories = [
+        { name: "growcast", elementId: "growcast" },
+        { name: "flutter", elementId: "flutter" },
+        { name: "ux-ui", elementId: "ux-ui" },
+        { name: "diversos", elementId: "diversos" }
+    ]
+
+    categories.forEach(({ name, elementId }) => {
+        const categoryCards = cardsArray.filter(obj => obj.category === name)
+        if (categoryCards.length > 0) {
+            const categoryElement = createCards(categoryCards)
+            document.getElementById(elementId).appendChild(categoryElement)
+        }
     })
 
-    const div = document.createElement("div")
-    div.classList.add("col")
-    div.innerHTML = card
-    container.appendChild(col)
 }
-
-//renderCards(objArray, containerId)
 
