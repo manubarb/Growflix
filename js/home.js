bf4ab8e2f7375a584cbdcf37ee1217c495b6a143
Manuela de Almeida Barba <manuelabarba@mpam.mp.br>
2025-11-28 14:06:35 -0400

    implementaçao do modal de videos

--- js/home.js
+++ js/home.js
@@ -8,7 +8,7 @@ function createCards(objArray) {
                 <div class="card-wrapper">
                     <img src="${img}" class="img-fluid" alt="${title}">
                     <div class="card-info">
-                        <a href="${link}" target="_blank" class="card-link">
+                        <a  class="card-link" data-link="${link}">
                             <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-play-circle" viewBox="0 0 16 16">
                             <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
                             <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445"/>
@@ -41,6 +41,7 @@ function renderCards(){
 
     categories.forEach(({ name, id }) => {
         const cardsByCategory = cardsArray.filter(card => card.category === name)
+        console.log(cardsByCategory)
         if (cardsByCategory.length > 0) {
             const category = createCards(cardsByCategory)
             document.getElementById(id).appendChild(category)
@@ -49,3 +50,26 @@ function renderCards(){
 
 }
 renderCards()   
+
+// funções para controlar o modal
+function openModal(link) {
+    document.getElementById('youtubePlayer').src = link
+    document.getElementById('youtubeModal').style.display = 'block'
+}
+
+function closeModal(){
+    document.querySelector('.close-btn').addEventListener('click', () => {
+    document.getElementById('youtubePlayer').src = ''
+    document.getElementById('youtubeModal').style.display = 'none'})
+}
+closeModal()
+
+function renderModal(){
+    const links = document.getElementsByClassName('card-link')
+    for(let i =0; i < links.length; i++){
+        links[i].addEventListener('click', function(){
+        const link = this.getAttribute('data-link')
+        openModal(link)
+    })}
+}
+renderModal()
